9a12250c4efaf7ab3aef941ac34ee695bb455675
Danil Borchevkin <danil.borchevkin@lab409.ru>
2017-12-05 01:46:34 +0300

    Add readme.md. Fix gitignore. Fix index router

--- portal.go
+++ portal.go
@@ -9,9 +9,10 @@ import (
 func main() {
 	addr := ":8081"
 
+	http.HandleFunc("/", handlers.IndexHandler)
+	http.HandleFunc("/index/", handlers.IndexHandler)	
 	http.HandleFunc("/api/wc/status", handlers.WcStatusHandler)
-	http.HandleFunc("/index/", handlers.IndexHandler)
-
+	
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	fmt.Printf("Listening on addr: %s\n", addr)
